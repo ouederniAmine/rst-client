382a8bbaf62227b5eb444b912446fc57394a8a94
ouederniAmine <amounaouederni@gmail.com>
2023-03-15 08:29:57 +0100

    f

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -62,6 +62,32 @@ const Sidebar = () => {
         navigate("/login");
     }
     const [open, setOpen] = useState(true);
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+        if (window.innerWidth < 768) {
+          setOpen(false);
+        }
+        if (window.innerWidth > 768) {
+          setOpen(true);
+        }
+      }
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
  
   return ( <div className="flex">
   <div
