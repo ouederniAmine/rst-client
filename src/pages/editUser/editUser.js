df032ad3f776f432ce848d3f63f54e92c1dee1ca
ouederniAmine <68029210+ouederniAmine@users.noreply.github.com>
2023-08-09 18:01:13 +0100

    Update editUser.js

--- src/pages/editUser/editUser.js
+++ src/pages/editUser/editUser.js
@@ -430,6 +430,7 @@ console.log(res.data[0])
                
 
     <Switch checked={data.auto_trader==="1"}
+color={autoTrader ? 'green' : 'red'}
   onChange={(e) => {
 console.log(data.auto_trader)
     setData({
