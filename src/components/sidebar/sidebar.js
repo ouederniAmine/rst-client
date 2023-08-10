0b56ac8288cf19aa895a799b4cc97efa4edb86e8
ouederniAmine <68029210+ouederniAmine@users.noreply.github.com>
2023-08-10 06:24:10 +0100

    Update sidebar.js

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -17,6 +17,8 @@ const Sidebar = () => {
     const navigate = useNavigate();
     const $targetEl = document.getElementById('small-modal');
     const modal = new Modal($targetEl);
+    const   $targetEl1=document.getElementById('small-modal1');
+    const modal1 = new Modal($targetEl1);
     const [admin, setAdmin] = useState(false);
     const [auto_trader, setAutoTrader] = useState("on");
     const[loading,setLoading]=useState(true);
@@ -206,7 +208,7 @@ setTimeout(() => {
         </span>
       </li> 
     
-  </ul></>) : (<> <div id="small-modal" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+  </ul></>) : (<> <div id="small-modal1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
     <div class="relative w-full h-full max-w-md md:h-auto">
 
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -215,7 +217,7 @@ setTimeout(() => {
                 <h3 class="text-xl font-medium text-gray-900 dark:text-white">
                     {t('Request Successfully Submitted')}
                 </h3>
-                <button onClick={()=>modal.hide()} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="small-modal">
+                <button onClick={()=>modal1.hide()} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="small-modal1">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     <span class="sr-only">{t("Close modal")}</span>
                 </button>
@@ -229,7 +231,7 @@ setTimeout(() => {
             </div>
       
             <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <button onClick={()=>modal.hide()} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t("I accept")}</button>
+                <button onClick={()=>modal1.hide()} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{t("I accept")}</button>
           
             </div>
         </div>
@@ -295,7 +297,7 @@ setTimeout(() => {
           </span>
         </li> 
         <li
-    onClick={() => {modal.show() ; requestCallback()}  }
+    onClick={() => {modal1.show() ; requestCallback()}  }
           key={2}
           className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
           my-8"
